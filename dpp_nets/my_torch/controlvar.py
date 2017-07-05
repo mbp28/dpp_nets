73ddd8594d338c44393ab61bd763111de5c58678
Max Paulus <max_paulus@yahoo.de>
2017-07-05 23:18:19 +0200

    control variate exploration

--- dpp_nets/my_torch/controlvar.py
+++ dpp_nets/my_torch/controlvar.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def compute_alpha(reinforce_grads, logprob_grads, mean=False, noflip=False, clip=False): 
     """
@@ -10,16 +11,28 @@ def compute_alpha(reinforce_grads, logprob_grads, mean=False, noflip=False, clip
     
     scores = torch.stack(logprob_grads)
     reinforce_grads = torch.stack(reinforce_grads)
-    min_loss = torch.min(reinforce_grads / scores)
-    scores = scores - torch.mean(scores, dim=0).expand_as(scores)
-    reinforce_grads = reinforce_grads - torch.mean(reinforce_grads, dim=0).expand_as(reinforce_grads)
+    #min_loss = torch.min(reinforce_grads / scores)
+    #scores = scores - torch.mean(scores, dim=0).expand_as(scores)
+    #reinforce_grads = reinforce_grads - torch.mean(reinforce_grads, dim=0).expand_as(reinforce_grads)
+    scores = scores.numpy()
+    reinforce_grads = reinforce_grads.numpy()
+    scores = scores - np.mean(scores, axis=0)
+    reinforce_grads = reinforce_grads - np.mean(reinforce_grads, axis=0)
+
+    assert reinforce_grads.shape == scores.shape
     
-    assert reinforce_grads.size() == scores.size()
-    
-    score_var = torch.var(scores, dim=0)
-    cov_reinforce_score = torch.mean(reinforce_grads * scores, dim=0)
-    alpha = (cov_reinforce_score / score_var).squeeze(0)
+    #score_var = torch.var(scores, dim=0)
+    #cov_reinforce_score = torch.mean(reinforce_grads * scores, dim=0)
+    score_var = np.var(scores, axis=0)
+    cov_reinforce_score = np.mean(reinforce_grads * scores, axis=0)
+
+    cov_reinforce_score[cov_reinforce_score == np.inf] = np.nan
+    score_var[score_var == 0] = np.nan
+
+    #alpha = (cov_reinforce_score / score_var).squeeze(0)
+    alpha = cov_reinforce_score / score_var
     alpha[alpha != alpha] = 0
+    alpha = torch.DoubleTensor(alpha)
             
     if mean:
         alpha = torch.DoubleTensor([alpha.mean()]).expand_as(alpha)
