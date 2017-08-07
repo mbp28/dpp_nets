44b3cb108b921dbbaa350301e1140b996e529faf
Max Paulus <max_paulus@yahoo.de>
2017-08-08 00:48:39 +0200

    Revert "train.py"
    This reverts commit e6f5a264c8c35b606677b40568d6beeccc8a801e.

--- train_baseline_beer_cpu.py
+++ train_baseline_beer_cpu.py
@@ -102,8 +102,7 @@ def main():
 
         #train(val_loader, model, criterion, optimizer, args.aspect)
         
-        #loss = validate(val_loader, model, criterion, args.aspect)
-        loss = 42
+        loss = validate(val_loader, model, criterion, args.aspect)
 
         log(epoch, loss)
 
