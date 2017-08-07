cddc4bff9a465c74d537b77c784eadf555870734
Max Paulus <max_paulus@yahoo.de>
2017-08-07 21:57:16 +0200

    test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+def main():
+	log('It worked here.')
+
+def log(msg):
+    
+    msg = str(msg)
+    path = '/cluster/home/paulusm/data/beer_reviews/test_file.txt'
+
+    with open(path, 'a') as log:
+        log.write(string + '\n')
+
+if __name__ == '__main__':
+    main()
