711cfbb94be194e2f16b92c4062e58ee8f2941a9
Max Paulus <max_paulus@yahoo.de>
2017-08-07 22:47:29 +0200

    test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-def main():
-    print("Executing.")
-    log('It worked here.')
-    print("Executed.")
+print("Executing.")
+log('It worked here.')
+print("Executed.")
 
 def log(msg):
-    
     msg = str(msg)
-    path = '/cluster/home/paulusm/data/beer_reviews/test_file.txt'
-
+    path = '/cluster/home/paulusm/checkpoints/beer_reviews/test_file.txt'
     with open(path, 'a') as log:
         log.write(msg)
 
