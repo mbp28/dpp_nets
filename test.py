1d6741e00d145931090813fd81c51c74d0c1c0cb
Max Paulus <max_paulus@yahoo.de>
2017-08-07 21:58:34 +0200

    test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def log(msg):
     path = '/cluster/home/paulusm/data/beer_reviews/test_file.txt'
 
     with open(path, 'a') as log:
-        log.write(string + '\n')
+        log.write(msg + '\n')
 
 if __name__ == '__main__':
     main()
